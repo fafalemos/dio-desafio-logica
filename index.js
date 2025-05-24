a66dec6639f666aa946f38dabdbc57caf63c9149
fafalemos <f.lemos@outlook.com.br>
2025-05-24 12:20:01 -0300

    Implementacao do sistema de avaliacao de heroi

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Olá mundo  ')
+const heroi = {};
+const { Console } = require('console');
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+});
+
+rl.question('Digite o nome do herói: ', (nome) => {
+    heroi.nome = nome;
+
+    rl.question('Digite sua experiencia: ', (xp) => {
+        heroi.xp = xp;
+
+        console.log('Olá,', heroi.nome);
+        heroi.avaliar();
+        rl.close();
+    })
+})
+
+
+heroi.avaliar = function avaliar(rank){
+
+if(heroi.xp <= 1000){
+    heroi.rank = 'Ferro';
+}if(heroi.xp > 1000 || heroi.xp <= 2000){
+    heroi.rank = 'Bronze';
+}if(heroi.xp > 2000 || heroi.xp <= 5000){
+    heroi.rank = 'Prata'
+}if(heroi.xp > 5000 || heroi.xp <= 7000){
+    heroi.rank = 'Ouro'
+}if(heroi.xp > 7000 || heroi.xp <= 8000){
+    heroi.rank = 'Platina'
+}if(heroi.xp > 8000 || heroi.xp <= 9000){
+    heroi.rank = 'Ascendente'
+}if(heroi.xp > 9000 || heroi.xp <= 10000){
+    heroi.rank =  'Imortal'
+}if(heroi.xp > 10000){
+    heroi.rank = 'Radiante'
+}
+
+console.log('O herói de nome', heroi.nome, 'está no nível', heroi.rank);
+}
